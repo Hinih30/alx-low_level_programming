28dfb005fd792053ae155ff8811be6e1706bcee5
Hinih30 <hinih30@gmail.com>
2023-10-10 19:48:55 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,11 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	typedef struct dog dog_t;
 
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
-	d->owner =owner;
+	d->owner = owner;
 }
