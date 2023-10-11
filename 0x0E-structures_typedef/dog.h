cbbe8324b4978b2876c908db29601a0d439cda51
Hinih30 <hinih30@gmail.com>
2023-10-10 18:51:44 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: The owner of the dog
+ *
+ * Description: Elements of the new type
+ * Return: Always 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
