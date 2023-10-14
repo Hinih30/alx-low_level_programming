c433beedb2eacb94a4f842067dd933cc7df064ff
Hinih30 <hinih30@gmail.com>
2023-10-14 04:55:19 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints a string followed by a new line
+ * @c string to be printed
+ *
+ * Return: Always 0.
+ */
+int _putchar(char c)
+{
+	return (_putchar (c));
+}
